239ab66cddfcea7fc966d6b971e44336ab99b6ad
ZeroZoa <roh100438@gmail.com>
2025-10-17 20:45:58 +0900

    dotenv수정

--- src/main/java/NPJ/Crewer/CrewerApplication.java
+++ src/main/java/NPJ/Crewer/CrewerApplication.java
@@ -1,6 +1,5 @@
 package NPJ.Crewer;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
